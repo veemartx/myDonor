243d7459e7b0f6879045bf8cb38858cce05351b0
vee <vmartxsm@gmail.com>
2021-05-10 13:40:49 +0300

    may 10th update

---
+++ server/getAppeal.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'connect.php';
+
+// sel 
+$appealId = $_POST['appealId'];
+
+$res = new stdClass();
+
+$sel = "SELECT * FROM appeals WHERE appealId='$appealId'";
+
+$selQuery = mysqli_query($con, $sel);
+
+if ($r = mysqli_fetch_assoc($selQuery)) {
+
+    $res->$code = $r['appealCode'];
+
+    $res->center = $r['center'];
+
+    $res->date = $r['dueDate'];
+
+    $res->created = $r['createdAt'];
+
+    $res->status = $r['status'];
+
+    $res->bloodType = $r['bloodtType'];
+
+    $user = $r['userId'];
+
+    $selu = "SELECT * FROM users WHERE userId='$user'";
+
+    $seluQuery = mysqli_query($con, $selu);
+
+    if ($u = mysqli_fetch_assoc($seluQuery)) {
+
+        $app->user = $u['fullname'];
+    }
+}
